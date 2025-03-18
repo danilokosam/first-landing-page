0072c43b21e3e9da919dac75140ffdddb7bf2a81
Joaquin <joaquinyapura@gmail.com>
2025-03-18 18:39:05 -0300

    jsx para iterar iconos

---
+++ src/icons.jsx
@@ -0,0 +1,20 @@
+import { FaFacebookF, FaTwitter } from "react-icons/fa";
+import { TiSocialInstagram } from "react-icons/ti";
+
+export const Icons = [
+  {
+    icon: <FaFacebookF />,
+    name: "facebook",
+    url: "http://facebook.com",
+  },
+  {
+    icon: <TiSocialInstagram />,
+    name: "instagram",
+    url: "http://instagram.com",
+  },
+  {
+    icon: <FaTwitter />,
+    name: "twitter",
+    url: "http://x.com",
+  },
+];
