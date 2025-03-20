8d250bf3df4b762e6163113dad3668693c64608d
Joaquin <joaquinyapura@gmail.com>
2025-03-20 11:27:18 -0300

    classes fixed in main layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,7 +5,7 @@ import { Navbar } from "../components/Navbar";
 
 export const MainLayout = () => {
   return (
-    <div>
+    <div className="mx-auto w-full">
       {/* Navigation */}
       <Navbar />
       <FlavorFull />
