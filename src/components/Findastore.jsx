ec6db23db13174479e749fc8a8f8b2218ab5fc00
Daniel Duarte <danielduartemora27@gmail.com>
2025-03-23 12:06:29 +0100

    Modificación de imagen en Findastore

--- src/components/Findastore.jsx
+++ src/components/Findastore.jsx
@@ -1,8 +1,13 @@
-import findus from "../assets/images/findus.webp";
+import mapbg from "../assets/images/map-bg.jpg";
 
 export const Findastore = () => {
   return (
-    <div style={{ backgroundImage: `url(${findus})` }}>
+    <div
+      style={{
+        backgroundImage: `url(${mapbg})`,
+        backgroundSize: "cover",
+      }}
+    >
       <div className="flex h-full w-full flex-col items-center justify-center py-10 text-center text-gray-100 backdrop-brightness-50">
         <h3 className="text-3xl font-bold md:text-5xl">Find a Store</h3>
         <p className="p-2 text-sm md:w-[600px] md:text-lg">
