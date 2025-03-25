0c71401b078e8b65e9a83ae349efe48a07bd0536
Daniel Duarte <danielduartemora27@gmail.com>
2025-03-25 13:04:05 +0100

    Cambios en footer

--- src/assets/icons/react-icons/Icons.jsx
+++ src/assets/icons/react-icons/Icons.jsx
@@ -8,27 +8,27 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 
 export const Icons = [
   {
-    icon: <FaFacebookF />,
+    icon: FaFacebookF,
     name: "facebook",
     url: "http://facebook.com",
   },
   {
-    icon: <FaInstagram />,
+    icon: FaInstagram,
     name: "instagram",
     url: "http://instagram.com",
   },
   {
-    icon: <FaSquareXTwitter />,
+    icon: FaSquareXTwitter,
     name: "x",
     url: "http://x.com",
   },
   {
-    icon: <FaSnapchat />,
+    icon: FaSnapchat,
     name: "snapchat",
     url: "http://snapchat.com",
   },
   {
-    icon: <FaWhatsapp />,
+    icon: FaWhatsapp,
     name: "whatsapp",
     url: "http://whatsapp.com",
   },
--- src/components/Footer2.jsx
+++ src/components/Footer2.jsx
@@ -3,30 +3,30 @@ import { Icons } from "../assets/icons/react-icons/icons";
 
 export const Footer2 = () => {
   return (
-    <footer className="bg-gradient-to-tl from-[#E3FF73] to-[#E27C39] py-4">
+    <footer className="bg-gradient-to-tl from-white to-[#E0E0E0] py-4">
       {/* Contenedor */}
-      <div className="container mx-auto flex items-center justify-center gap-2">
-        <p className="text-md leading-none font-extrabold text-[#7E5232] md:text-2xl md:text-lg">
+      <div className="container mx-auto flex items-center justify-center gap-10">
+        <p className="text-md leading-none font-extrabold text-[#7E5232] md:text-lg">
           STAY <span className="block w-full">CONNECTED!</span>
         </p>
 
         <ul className="flex items-center justify-center space-x-2 md:space-x-3 lg:space-x-1">
-          {Icons.map((item) => (
-            <li key={item.name} className="rounded-full bg-[#7E5232] p-2">
-              <a
-                href={item.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label={item.name}
-                className="text-white"
-              >
-                {React.cloneElement(item.icon, {
-                  className:
-                    "h-6 w-6 transition-transform duration-500 ease-in-out hover:scale-110",
-                })}
-              </a>
-            </li>
-          ))}
+          {Icons.map((item) => {
+            const IconComponent = item.icon;
+            return (
+              <li key={item.name} className="rounded-full bg-[#7E5232] p-2">
+                <a
+                  href={item.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label={item.name}
+                  className="text-white"
+                >
+                  <IconComponent className="h-6 w-6 transition-transform duration-500 ease-in-out hover:scale-110" />
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </footer>
